e5eca11af3f8968bf35188a77442020d26b7ad7b
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2019-07-20 11:53:31 +0900

    詳細ページ作成、遷移もできた

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_yaimafuni/detail_page.dart';
 import 'package:flutter_yaimafuni/model/lesson.dart';
 
 void main() => runApp(new MyApp());
@@ -38,6 +39,73 @@ class _ListPageState extends State<ListPage> {
 
   @override
   Widget build(BuildContext context) {
+    /// リストタイトル
+    ListTile makeListTile(Lesson lesson) => ListTile(
+          contentPadding:
+              EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          leading: Container(
+            padding: EdgeInsets.only(right: 12.0),
+            decoration: new BoxDecoration(
+                border: new Border(
+                    right: new BorderSide(width: 1.0, color: Colors.white24))),
+            child: Icon(Icons.autorenew, color: Colors.white),
+          ),
+          title: Text(
+            lesson.title,
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          subtitle: Row(
+            children: <Widget>[
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    // tag: 'hero',
+                    child: LinearProgressIndicator(
+                        backgroundColor: Color.fromRGBO(209, 224, 224, 0.2),
+                        value: lesson.indicatorValue,
+                        valueColor: AlwaysStoppedAnimation(Colors.green)),
+                  )),
+              Expanded(
+                flex: 4,
+                child: Padding(
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Text(lesson.level,
+                        style: TextStyle(color: Colors.white))),
+              )
+            ],
+          ),
+          trailing:
+              Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => DetailPage(lesson: lesson)));
+          },
+        );
+
+    /// カード
+    Card makeCard(Lesson lesson) => Card(
+          elevation: 8.0,
+          margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+          child: Container(
+            decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+            child: makeListTile(lesson),
+          ),
+        );
+
+    /// ボディ
+    final makeBody = Container(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: 7,
+        itemBuilder: (BuildContext context, int index) {
+          return makeCard(lessons[index]);
+        },
+      ),
+    );
+
     return Scaffold(
       backgroundColor: Color.fromRGBO(76, 175, 202, 1.0),
       appBar: topAppBar,
@@ -52,81 +120,4 @@ final topAppBar = AppBar(
   elevation: 0.1,
   backgroundColor: Color.fromRGBO(76, 175, 202, 1.0),
   title: Text("やいまふに"),
-//  actions: <Widget>[
-//    IconButton(
-//      icon: Icon(Icons.list),
-//      onPressed: () {},
-//    )
-//  ],
 );
-
-/// ボディ
-final makeBody = Container(
-  child: ListView.builder(
-    scrollDirection: Axis.vertical,
-    shrinkWrap: true,
-    itemCount: 7,
-    itemBuilder: (BuildContext context, int index) {
-      return makeCard;
-    },
-  ),
-);
-
-/// カード
-final makeCard = Card(
-  elevation: 8.0,
-  margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-  child: Container(
-    decoration: BoxDecoration(color: Colors.white),
-    child: makeListTile,
-  ),
-);
-
-/// リストタイトル
-final makeListTile = ListTile(
-    contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-    leading: Container(
-      padding: EdgeInsets.only(right: 12.0),
-      decoration: new BoxDecoration(
-          border: new Border(
-              right: new BorderSide(width: 1.0, color: Colors.grey))),
-      child: Icon(Icons.autorenew, color: Colors.grey), //todo アイコンを動的に変更
-    ),
-    title: Text(
-      "XX航路",
-      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-    ),
-    // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
-
-    subtitle: Text("通常運行", style: TextStyle(color: Colors.grey)),
-    trailing:
-        Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
-
-///// ボトムナビバー
-//final makeBottom = Container(
-//  height: 55.0,
-//  child: BottomAppBar(
-//    color: Color.fromRGBO(58, 66, 86, 1.0),
-//    child: Row(
-//      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//      children: <Widget>[
-//        IconButton(
-//          icon: Icon(Icons.home, color: Colors.white),
-//          onPressed: () {},
-//        ),
-//        IconButton(
-//          icon: Icon(Icons.blur_on, color: Colors.white),
-//          onPressed: () {},
-//        ),
-//        IconButton(
-//          icon: Icon(Icons.hotel, color: Colors.white),
-//          onPressed: () {},
-//        ),
-//        IconButton(
-//          icon: Icon(Icons.account_box, color: Colors.white),
-//          onPressed: () {},
-//        )
-//      ],
-//    ),
-//  ),
-//);
