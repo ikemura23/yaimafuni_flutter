0cdd63fab3bc07f1e44940a3b52e323a5f277a10
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2019-07-20 14:56:38 +0900

    リストの作り変え中

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,41 @@ class _ListPageState extends State<ListPage> {
 
   @override
   Widget build(BuildContext context) {
+
+    // リスト行の作成
+    Widget _buildButtonColumn(IconData icon, String label) {
+      final color = Theme.of(context).primaryColor;
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(icon, color: color), // 3.1.1
+          Container(
+            // 3.1.2
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              label,
+              style: TextStyle(
+                  fontSize: 12.0, fontWeight: FontWeight.w400, color: color),
+            ),
+          )
+        ],
+      );
+    }
+
+    Widget createListRow() => Container(
+        margin: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+        child: Row(
+          // 1行目
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _buildButtonColumn(Icons.call, "CALL"), // 2.1
+            _buildButtonColumn(Icons.near_me, "ROUTE"), // 2.2
+            _buildButtonColumn(Icons.share, "SHARE") // 2.3
+          ],
+        ));
+
     /// リストタイトル
     ListTile makeListTile(Status status) => ListTile(
           contentPadding:
@@ -91,7 +126,8 @@ class _ListPageState extends State<ListPage> {
           margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
           child: Container(
             decoration: BoxDecoration(color: Colors.white),
-            child: makeListTile(status),
+//            child: makeListTile(status),
+            child: createListRow(),
           ),
         );
 
