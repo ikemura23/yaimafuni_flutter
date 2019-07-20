382a504bc7651b0f05e8e97ef7c54ca5efd1fdf0
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2019-07-20 09:37:48 +0900

    リストが表示できた

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _ListPageState extends State<ListPage> {
     return Scaffold(
       backgroundColor: Color.fromRGBO(76, 175, 202, 1.0),
       appBar: topAppBar,
-      body: ,
+      body: makeBody,
     );
   }
 }
@@ -48,3 +48,47 @@ final topAppBar = AppBar(
     )
   ],
 );
+
+final makeBody = Container(
+  child: ListView.builder(
+    scrollDirection: Axis.vertical,
+    shrinkWrap: true,
+    itemCount: 10,
+    itemBuilder: (BuildContext context, int index) {
+      return makeCard;
+    },
+  ),
+);
+
+final makeCard = Card(
+  elevation: 8.0,
+  margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+  child: Container(
+    decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+    child: makeListTile,
+  ),
+);
+
+final makeListTile = ListTile(
+    contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+    leading: Container(
+      padding: EdgeInsets.only(right: 12.0),
+      decoration: new BoxDecoration(
+          border: new Border(
+              right: new BorderSide(width: 1.0, color: Colors.white24))),
+      child: Icon(Icons.autorenew, color: Colors.white),
+    ),
+    title: Text(
+      "Introduction to Driving",
+      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+    ),
+    // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+    subtitle: Row(
+      children: <Widget>[
+        Icon(Icons.linear_scale, color: Colors.yellowAccent),
+        Text(" Intermediate", style: TextStyle(color: Colors.white))
+      ],
+    ),
+    trailing:
+        Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
